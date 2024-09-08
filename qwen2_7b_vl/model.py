1338b30e4bfdd30379841192b1f7985644d62ed8
Bi Chen <878788+crazyseabiscuit@users.noreply.github.com>
2024-09-08 18:30:31 +0800

    qwen

---
+++ qwen2_7b_vl/model.py
@@ -0,0 +1,7 @@
+from transformers import Qwen2VLForConditionalGeneration, AutoTokenizer, AutoProcessor
+from qwen_vl_utils import process_vision_info
+
+# default: Load the model on the available device(s)
+model = Qwen2VLForConditionalGeneration.from_pretrained(
+    "Qwen/Qwen2-VL-7B-Instruct", torch_dtype="auto", device_map="auto"
+)
